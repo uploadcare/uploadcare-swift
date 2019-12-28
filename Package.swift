15c67ebf4a75af2b0498b61983620a86242bde3a
Sergey Armodin <dolph-in@yandex.ru>
2019-12-28 14:20:12 +0300

    rename

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "uploadcare-swift",
+            name: "UploadcareSwift",
             targets: ["uploadcare-swift"]),
     ],
     dependencies: [
