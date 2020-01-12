47c0f8c049fd859fef637c38ff56fbc9217b21d3
Sergey Armodin <dolph-in@yandex.ru>
2020-01-12 20:52:19 +0300

    demo app added

---
+++ DemoApp/DemoApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Sergey Armodin on 12.01.2020.
+//  Copyright © 2020 Sergei Armodin. All rights reserved.
+//
+
+import UIKit
+import Uploadcare
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		Uploadcare.sayHi()
+	}
+
+
+}
+
