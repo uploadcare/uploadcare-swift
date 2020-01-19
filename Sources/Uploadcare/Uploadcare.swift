dbfe93f3d1debc7efa254e03e2ab1379f0c73cd4
Sergey Armodin <dolph-in@yandex.ru>
2020-01-19 17:42:51 +0300

    fileInfo api method

--- Sources/Uploadcare/Uploadcare.swift
+++ Sources/Uploadcare/Uploadcare.swift
@@ -27,3 +27,59 @@ public struct Uploadcare {
 		print("Uploadcare says Hi!")
 	}
 }
+
+
+private extension Uploadcare {
+	func makeError(fromResponse response: DataResponse<Data>) -> Error {
+		let status: Int = response.response?.statusCode ?? 0
+		
+		var message = ""
+		if let data = response.data {
+			message = String(data: data, encoding: .utf8) ?? ""
+		}
+		
+		return Error(status: status, message: message)
+	}
+}
+
+
+extension Uploadcare {
+	public func fileInfo(
+		withFileId fileId: String,
+		_ completionHandler: @escaping (FileInfo?, Error?) -> Void
+	) {
+		let urlString = uploadAPIBaseUrl + "/info?pub_key=\(self.publicKey)&file_id=\(fileId)"
+		guard let url = URL(string: urlString) else { return }
+		var urlRequest = URLRequest(url: url)
+		urlRequest.httpMethod = HTTPMethod.get.rawValue
+		
+		request(urlRequest)
+			.validate(statusCode: 200..<300)
+			.responseData { response in
+				switch response.result {
+				case .success(let data):
+					let decodedData = try? JSONDecoder().decode(FileInfo.self, from: data)
+		
+					guard let fileInfo = decodedData else {
+						completionHandler(nil, Error.defaultError())
+						return
+					}
+
+					completionHandler(fileInfo, nil)
+				case .failure(_):
+					let error = self.makeError(fromResponse: response)
+					completionHandler(nil, error)
+				}
+		}
+	}
+}
+
+
+//public protocol Uploadable {
+//	func directUpload()
+//}
+
+
+//extension Uploadcare: Uploadable {
+//	func directUpload()
+//}
--- Sources/Uploadcare/models/Error.swift
+++ Sources/Uploadcare/models/Error.swift
@@ -11,4 +11,8 @@ import Foundation
 public struct Error {
 	public var status: Int
 	public var message: String
+	
+	public static func defaultError() -> Error {
+		return Error(status: 0, message: "Unknown error")
+	}
 }
