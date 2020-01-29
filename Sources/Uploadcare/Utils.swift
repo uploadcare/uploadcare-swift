446e84d1f84d1f4092267add4da11005518895a1
Sergey Armodin <dolph-in@yandex.ru>
2020-01-30 02:11:15 +0300

    mimeType util func

--- Sources/Uploadcare/Utils.swift
+++ Sources/Uploadcare/Utils.swift
@@ -23,3 +23,30 @@ func DLog(_ messages: Any..., fullPath: String = #file, line: Int = #line, funct
 	}
 	#endif
 }
+
+
+/// Get mime type from Data
+/// - Parameter data: data
+func mimeType(for data: Data) -> String {
+	var b: UInt8 = 0
+	data.copyBytes(to: &b, count: 1)
+
+	switch b {
+	case 0xFF:
+		return "image/jpeg"
+	case 0x89:
+		return "image/png"
+	case 0x47:
+		return "image/gif"
+	case 0x4D, 0x49:
+		return "image/tiff"
+	case 0x25:
+		return "application/pdf"
+	case 0xD0:
+		return "application/vnd"
+	case 0x46:
+		return "text/plain"
+	default:
+		return "application/octet-stream"
+	}
+}
