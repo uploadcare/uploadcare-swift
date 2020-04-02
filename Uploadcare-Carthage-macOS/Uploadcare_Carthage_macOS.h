6165146bf05397bf9f01719c69e8f61cd4c3b8fa
Sergey Armodin <dolph-in@yandex.ru>
2020-04-02 17:02:37 +0300

    wip

---
+++ Uploadcare-Carthage-macOS/Uploadcare_Carthage_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Uploadcare_Carthage_macOS.h
+//  Uploadcare-Carthage-macOS
+//
+//  Created by Sergey Armodin on 02.04.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Uploadcare_Carthage_macOS.
+FOUNDATION_EXPORT double Uploadcare_Carthage_macOSVersionNumber;
+
+//! Project version string for Uploadcare_Carthage_macOS.
+FOUNDATION_EXPORT const unsigned char Uploadcare_Carthage_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Uploadcare_Carthage_macOS/PublicHeader.h>
+
+
