adbdf449c8c9156bf6f9cf55afb6611679d1137a
Sergey Armodin <dolph-in@yandex.ru>
2020-04-06 12:33:38 +0300

    wip

---
+++ Uploadcare-watchOS/Uploadcare_watchOS.h
@@ -0,0 +1,18 @@
+//
+//  Uploadcare_watchOS.h
+//  Uploadcare-watchOS
+//
+//  Created by Sergey Armodin on 06.04.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Uploadcare_watchOS.
+FOUNDATION_EXPORT double Uploadcare_watchOSVersionNumber;
+
+//! Project version string for Uploadcare_watchOS.
+FOUNDATION_EXPORT const unsigned char Uploadcare_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Uploadcare_watchOS/PublicHeader.h>
+
+
