0d26231733db320f9151d91fe9c4f00867367e56
Sergey Armodin <dolph-in@yandex.ru>
2020-04-06 13:00:09 +0300

    ...

---
+++ Uploadcare-macOS/Uploadcare_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Uploadcare_macOS.h
+//  Uploadcare-macOS
+//
+//  Created by Sergey Armodin on 06.04.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Uploadcare_macOS.
+FOUNDATION_EXPORT double Uploadcare_macOSVersionNumber;
+
+//! Project version string for Uploadcare_macOS.
+FOUNDATION_EXPORT const unsigned char Uploadcare_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Uploadcare_macOS/PublicHeader.h>
+
+
