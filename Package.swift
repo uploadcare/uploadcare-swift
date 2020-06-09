cf87d902178aaeeaeb8ba706c7268e1bc0f36456
Sergey Armodin <dolph-in@yandex.ru>
2020-06-09 22:15:07 +0300

    disable tests for watchOS because XCTest is unavailable for watchOS

--- Package.swift
+++ Package.swift
@@ -25,9 +25,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Uploadcare",
-            dependencies: ["Alamofire"]),
+            dependencies: ["Alamofire"]
+		),
         .testTarget(
             name: "UploadcareTests",
-            dependencies: ["Uploadcare"])
+            dependencies: ["Uploadcare"]
+		)
     ]
 )
--- Tests/UploadcareTests/XCTestManifests.swift
+++ Tests/UploadcareTests/XCTestManifests.swift
@@ -1,3 +1,4 @@
+#if !os(watchOS)
 import XCTest
 
 #if !canImport(ObjectiveC)
@@ -6,4 +7,5 @@ public func allTests() -> [XCTestCaseEntry] {
         testCase(uploadcare_swiftTests.allTests),
     ]
 }
+#endif
 #endif
