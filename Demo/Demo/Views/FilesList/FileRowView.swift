de386fcc23a3fe90d1f287d9e1c10f922cf484bb
Sergey Armodin <dolph-in@yandex.ru>
2020-06-26 20:23:41 +0300

    thumbnails in files list

--- Demo/Demo/Views/FilesList/FileRowView.swift
+++ Demo/Demo/Views/FilesList/FileRowView.swift
@@ -12,23 +12,58 @@ import Uploadcare
 struct FileRowView: View {
 	var fileData: FileViewData
 	
+	@State private var image: UIImage?
+	
     var body: some View {
 		NavigationLink(destination: FileView(fileData: fileData)) {
 			HStack {
 				if fileData.file.isImage {
-					Image(systemName: "photo.fill")
+					if self.image != nil {
+						Image(uiImage: self.image!)
+							.frame(width: 30, height: 30, alignment: .center)
+					} else {
+						Image(systemName: "photo.fill")
+							.frame(width: 30, height: 30, alignment: .center)
+					}
 				} else if fileData.file.videoInfo != nil {
 					Image(systemName: "video.fill")
+						.frame(width: 30, height: 30, alignment: .center)
 				} else {
 					Image(systemName: "doc.fill")
+						.frame(width: 30, height: 30, alignment: .center)
 				}
 				VStack(alignment: .leading) {
 					Text(fileData.file.originalFilename).font(.headline)
 					Text("size: \(fileData.file.size / 1024) kb").font(.subheadline)
 				}
 			}
+		}.onAppear {
+			if fileData.file.isImage {
+				self.loadImage()
+			}
 		}
     }
+	
+	private func loadImage() {
+		guard let imageUrl = self.fileData.file.originalFileUrl, var url = URL(string: imageUrl) else { return }
+		var str = url.deletingLastPathComponent().absoluteString
+		str = str + "-/scale_crop/30x30/smart/"
+		
+		print(str)
+		
+		url = URL(string: str)!
+		
+		DispatchQueue.global(qos: .utility).async {
+			guard let data = try? Data(contentsOf: url, options: .mappedIfSafe) else { return }
+			
+			let image = UIImage(data: data)
+			DispatchQueue.main.async {
+				withAnimation {
+					self.image = image
+				}
+			}
+		}
+	}
 }
 
 struct FileRowView_Previews: PreviewProvider {
