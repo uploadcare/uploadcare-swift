d8a50ad62e308326920792275ec8c8243a88272d
Sergey Armodin <dolph-in@yandex.ru>
2020-08-03 23:49:56 +0300

    added some modes for document convertion

---
+++ Sources/Uploadcare/models/REST/convertion/DocumentTargetFormat.swift
@@ -0,0 +1,22 @@
+//
+//  DocumentTargetFormat.swift
+//  
+//
+//  Created by Sergei Armodin on 03.08.2020.
+//
+
+import Foundation
+
+public enum DocumentTargetFormat: String {
+	case doc
+	case docx
+	case xls
+	case xlsx
+	case odt
+	case ods
+	case rtf
+	case txt
+	case pdf // default
+	case jpg
+	case png
+}
