d08c0ea50c127e020b955822eec6846f105dfe7c
Sergey Armodin <dolph-in@yandex.ru>
2020-08-25 18:21:21 +0300

    models updated

--- Sources/Uploadcare/models/REST/responses/ConvertDocumentsResponse.swift
+++ Sources/Uploadcare/models/REST/responses/ConvertDocumentsResponse.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct ConvertDocumentsResponse: Codable {
 	/// Dictionary of problems related to your processing job, if any. Key is the path you requested.
-	public let problems: [[String: String]]
+	public let problems: [String: String]
 	
 	/// Result for each requested path, in case of no errors for that path.
 	public let result: [DocumentConvertionJob]
@@ -24,7 +24,7 @@ public struct ConvertDocumentsResponse: Codable {
 	public init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		
-		problems = try container.decodeIfPresent([[String: String]].self, forKey: .problems) ?? []
+		problems = try container.decodeIfPresent([String: String].self, forKey: .problems) ?? [:]
 		result = try container.decodeIfPresent([DocumentConvertionJob].self, forKey: .result) ?? []
 	}
 }
