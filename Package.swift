8512d8c10752c21bc620bad9b21ea6327ed0eac0
Sergey Armodin <dolph-in@yandex.ru>
2020-11-19 18:56:48 +0300

    podspec updated

--- Package.swift
+++ Package.swift
@@ -12,12 +12,9 @@ let package = Package(
 		.watchOS(.v5)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "Uploadcare", targets: ["Uploadcare"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
 		.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.4")
     ],
     targets: [
