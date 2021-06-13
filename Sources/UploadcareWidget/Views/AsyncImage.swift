09e25292ae372237f2c242804fd9ba12707ad283
Sergey Armodin <dolph-in@yandex.ru>
2021-06-14 00:28:46 +0300

    images loading and caching in widget

---
+++ Sources/UploadcareWidget/Views/AsyncImage.swift
@@ -0,0 +1,56 @@
+//
+//  AsyncImage.swift
+//  
+//
+//  Created by Sergey Armodin on 14.06.2021.
+//  Copyright © 2021 Uploadcare, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 14.0.0, OSX 10.15.0, *)
+struct AsyncImage<Placeholder: View>: View {
+	@StateObject private var loader: ImageLoader
+	private let placeholder: Placeholder
+	private let image: (UIImage) -> Image
+
+	init(
+		url: URL,
+		@ViewBuilder placeholder: () -> Placeholder,
+		@ViewBuilder image: @escaping (UIImage) -> Image = Image.init(uiImage:)
+	) {
+		self.placeholder = placeholder()
+		self.image = image
+		_loader = StateObject(wrappedValue: ImageLoader(
+			url: url,
+			cache: Environment(\.imageCache).wrappedValue)
+		)
+	}
+
+	var body: some View {
+		content
+			.onAppear(perform: loader.load)
+	}
+
+	private var content: some View {
+		Group {
+			if loader.image != nil {
+				image(loader.image!)
+			} else {
+				placeholder
+			}
+		}
+	}
+}
+
+struct ImageCacheKey: EnvironmentKey {
+	static let defaultValue: ImageCache = ImageCache()
+}
+
+@available(iOS 13.0.0, OSX 10.15.0, *)
+extension EnvironmentValues {
+	var imageCache: ImageCache {
+		get { self[ImageCacheKey.self] }
+		set { self[ImageCacheKey.self] = newValue }
+	}
+}
--- Sources/UploadcareWidget/Views/SelectFileView.swift
+++ Sources/UploadcareWidget/Views/SelectFileView.swift
@@ -8,37 +8,27 @@
 
 import SwiftUI
 
-@available(iOS 13.0.0, OSX 10.15.0, *)
+@available(iOS 14.0.0, OSX 10.15.0, *)
 struct SelectFileView: View {
 	let thing: ChunkThing
 	@State private var image: UIImage?
 	let size: CGFloat
 	
     var body: some View {
-		HStack(spacing: 0) {
-			if let image = self.image {
-				Image(uiImage: image)
-					.resizable()
-					.aspectRatio(contentMode: .fill)
-					.frame(width: size - 12, height: size - 12, alignment: .center)
-					.clipped()
-			} else {
-				Image(systemName: "doc.fill")
-					.frame(width: size - 12, height: size - 12, alignment: .center)
-			}
-		}.onAppear {
-			guard let url = URL(string: self.thing.thumbnail) else { return }
-
-			DispatchQueue.global(qos: .utility).async {
-				guard let data = try? Data(contentsOf: url, options: .mappedIfSafe) else { return }
-
-				let image = UIImage(data: data)
-				DispatchQueue.main.async {
-					withAnimation {
-						self.image = image
-					}
+		if let url = URL(string: self.thing.thumbnail) {
+			AsyncImage(
+				url: url,
+				placeholder: {
+					Image(systemName: "doc.fill")
+				},
+				image: {
+					Image(uiImage: $0)
+						.resizable()
 				}
-			}
+			)
+			.frame(width: size - 12, height: size - 12, alignment: .center)
+			.aspectRatio(contentMode: .fill)
+			.clipped()
 		}
     }
 }
