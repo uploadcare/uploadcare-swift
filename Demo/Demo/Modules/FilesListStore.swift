6d0abd85cfcf38748f9a82e8e375eee99194b487
Sergei Armodin <dolph-in@yandex.ru>
2021-09-25 15:05:28 +0300

    using new upload method in demo app which decides internally if direct or multipart upload should be used

--- Demo/Demo/Modules/FilesListStore.swift
+++ Demo/Demo/Modules/FilesListStore.swift
@@ -87,7 +87,7 @@ class FilesListStore: ObservableObject {
 		
 	}
 	
-	func uploadFile(_ url: URL, completionHandler: @escaping (String)->Void) {
+	func uploadFile(_ url: URL, completionHandler: @escaping (String) -> Void) {
 		let data: Data
 		do {
 			data = try Data(contentsOf: url)
@@ -99,63 +99,24 @@ class FilesListStore: ObservableObject {
 		self.progressValue = 0
 		let filename = url.lastPathComponent
 
-		if data.count < UploadAPI.multipartMinFileSize {
-			self.performDirectUpload(filename: filename, data: data, completionHandler: completionHandler)
-		} else {
-			self.performMultipartUpload(filename: filename, fileUrl: url, completionHandler: completionHandler)
-		}
-	}
-	
-	func performDirectUpload(filename: String, data: Data, completionHandler: @escaping (String)->Void) {
-		let onProgress: (Double)->Void = { (progress) in
+		let onProgress: (Double) -> Void = { (progress) in
 			DispatchQueue.main.async { [weak self] in
 				self?.progressValue = progress
 			}
 		}
-		self.uploadcare?.uploadAPI.upload(files: [filename: data], store: .doNotStore, onProgress, { (uploadData, error) in
-			if let error = error {
-				return DLog(error)
-			}
 
-			guard let uploadData = uploadData, let fileId = uploadData.first?.value else { return }
-			completionHandler(fileId)
-			DLog(uploadData)
-		})
-	}
-	
-	func performMultipartUpload(filename: String, fileUrl: URL, completionHandler: @escaping (String)->Void) {
-		let onProgress: (Double)->Void = { (progress) in
-			DispatchQueue.main.async { [weak self] in
-				self?.progressValue = progress
-				
-//				switch UIApplication.shared.applicationState {
-//				case .background:
-//					let remain = UIApplication.shared.backgroundTimeRemaining.rounded(toPlaces: 2)
-//					DLog("Background time remaining = \(remain) seconds")
-//				default: break
-//				}
-			}
-		}
-
-		guard let fileForUploading = self.uploadcare?.uploadAPI.file(withContentsOf: fileUrl) else {
-			assertionFailure("file not found")
-			return
-		}
-		
-		self.currentTask = fileForUploading.upload(withName: filename, store: .doNotStore, onProgress, { (file, error) in
-			defer {
-				self.currentTask = nil
-			}
-			
+		self.currentTask = self.uploadcare?.uploadFile(data, withName: filename, store: .doNotStore, onProgress, { file, error in
 			if let error = error {
 				DLog(error)
 				return
 			}
-			
-			guard let file = file else { return }
-			completionHandler(file.fileId)
-			DLog(file)
-		})
+
+			guard let file = file else {
+				DLog("error: no file")
+				return
+			}
+			completionHandler(file.uuid)
+		}) as? UploadTaskResumable
 	}
 }
 
