2258b25b2f7e0f24092086a4f23982d30e8b18d0
Sergei Armodin <dolph-in@yandex.ru>
2022-02-01 01:03:37 +0300

    tests updated

--- Tests/UploadcareTests/UploadAPIIntegrationTests.swift
+++ Tests/UploadcareTests/UploadAPIIntegrationTests.swift
@@ -9,35 +9,6 @@
 import XCTest
 @testable import Uploadcare
 
-func DLog(
-	_ messages: Any...,
-	fullPath: String = #file,
-	line: Int = #line,
-	functionName: String = #function
-) {
-	let file = URL(fileURLWithPath: fullPath)
-	for message in messages {
-		#if DEBUG
-		let string = "\(file.pathComponents.last!):\(line) -> \(functionName): \(message)"
-		print(string)
-		#endif
-	}
-}
-
-/// Count size of Data (in mb)
-/// - Parameter data: data
-func sizeString(ofData data: Data) -> String {
-	let bcf = ByteCountFormatter()
-	bcf.allowedUnits = [.useMB] // optional: restricts the units to MB only
-	bcf.countStyle = .file
-	return bcf.string(fromByteCount: Int64(data.count))
-}
-
-func delay(_ delay: Double, closure: @escaping ()->()) {
-	DispatchQueue.main.asyncAfter(
-		deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
-}
-
 final class IntegrationTests: XCTestCase {
 	let uploadcare = Uploadcare(withPublicKey: "demopublickey", secretKey: "demopublickey")
 
