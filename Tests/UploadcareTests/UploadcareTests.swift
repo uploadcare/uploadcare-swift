8e7ed0e290df1c407281ed8f4da1e7f9e9316f83
Sergei Armodin <dolph-in@yandex.ru>
2022-04-29 00:05:51 +0300

    formatting

--- Tests/UploadcareTests/UploadcareTests.swift
+++ Tests/UploadcareTests/UploadcareTests.swift
@@ -3,30 +3,30 @@ import XCTest
 @testable import Uploadcare
 
 final class uploadcare_swiftTests: XCTestCase {
-    func testInitWithKeys() {
+	func testInitWithKeys() {
 		let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 		let randomGeneratedPublicKey = String((0..<10).map{ _ in letters.randomElement()! })
 		let randomGeneratedSecretKey = String((0..<10).map{ _ in letters.randomElement()! })
 		
 		
-        let uploadcare = Uploadcare(withPublicKey: randomGeneratedPublicKey, secretKey: randomGeneratedSecretKey)
+		let uploadcare = Uploadcare(withPublicKey: randomGeneratedPublicKey, secretKey: randomGeneratedSecretKey)
 		XCTAssertEqual(randomGeneratedPublicKey, uploadcare.publicKey)
 		XCTAssertEqual(randomGeneratedSecretKey, uploadcare.secretKey)
-    }
+	}
 	
 	func testRequestMethod() {
 		let uploadcare = Uploadcare(withPublicKey: "123", secretKey: "123")
 		
 		let urlString = "https://uploadcare.com"
 		let url = URL(string: urlString)!
-        let request = uploadcare.makeUrlRequest(fromURL: url, method: RequestManager.HTTPMethod.put)
+		let request = uploadcare.makeUrlRequest(fromURL: url, method: RequestManager.HTTPMethod.put)
 		
 		XCTAssertEqual(request.httpMethod, "PUT")
 	}
-
-    static var allTests = [
-        ("testInitWithKeys", testInitWithKeys),
+	
+	static var allTests = [
+		("testInitWithKeys", testInitWithKeys),
 		("testRequestMethod", testRequestMethod)
-    ]
+	]
 }
 #endif
