5ce0b5d741d635f1760fde9535dec1ec7f3d310b
Sergei Armodin <dolph-in@yandex.ru>
2023-09-01 00:34:50 +0300

    wip

--- Package.swift
+++ Package.swift
@@ -6,48 +6,48 @@ import PackageDescription
 #if os(Linux)
 let dependencies: [PackageDescription.Package.Dependency] = [
 	.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.18.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0")
+	.package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0")
 ]
 let targetDependencies: [PackageDescription.Target.Dependency] = [
 	.product(name: "AsyncHTTPClient", package: "async-http-client"),
-    .product(name: "Crypto", package: "swift-crypto")
+	.product(name: "Crypto", package: "swift-crypto")
 ]
 let products: [PackageDescription.Product] = [
-    .library(name: "Uploadcare", targets: ["Uploadcare"])
+	.library(name: "Uploadcare", targets: ["Uploadcare"])
 ]
 #else
 let dependencies: [PackageDescription.Package.Dependency] = []
 let targetDependencies: [PackageDescription.Target.Dependency] = []
 let products: [PackageDescription.Product] = [
-    .library(name: "Uploadcare", targets: ["Uploadcare"]),
+	.library(name: "Uploadcare", targets: ["Uploadcare"]),
 	.library(name: "UploadcareWidget", targets: ["UploadcareWidget"])
 ]
 #endif
 
 let package = Package(
-    name: "Uploadcare",
+	name: "Uploadcare",
 	platforms: [
 		.macOS(.v10_13),
 		.iOS(.v11),
 		.tvOS(.v11),
 		.watchOS(.v5)
-    ],
-    products: products,
-    dependencies: dependencies,
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Uploadcare",
-            dependencies: targetDependencies
+	],
+	products: products,
+	dependencies: dependencies,
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(
+			name: "Uploadcare",
+			dependencies: targetDependencies
 		),
 		.target(
 			name: "UploadcareWidget",
 			dependencies: ["Uploadcare"]
 		),
-        .testTarget(
-            name: "UploadcareTests",
-            dependencies: ["Uploadcare"]
+		.testTarget(
+			name: "UploadcareTests",
+			dependencies: ["Uploadcare"]
 		)
-    ]
+	]
 )
