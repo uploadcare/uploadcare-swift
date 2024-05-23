15ec8af2ff7da49f46461a43cd341a844c545ec0
Sergey Armodin <dolph-in@yandex.ru>
2024-05-23 23:15:07 +0300

    Integration test for documentInfo method added

--- Tests/UploadcareTests/RESTAPIIntegrationAsyncTests.swift
+++ Tests/UploadcareTests/RESTAPIIntegrationAsyncTests.swift
@@ -560,4 +560,27 @@ final class RESTAPIIntegrationAsyncTests: XCTestCase {
 		let status = try await uploadcare.performRemoveBG(fileUUID: uuid, parameters: parameters)
 		XCTAssertTrue(status.status != .unknown)
 	}
+
+	func test31_document_info() async throws {
+		let url = URL(string: "https://source.unsplash.com/featured")!
+		let data = try! Data(contentsOf: url)
+		DLog("size of file: \(sizeString(ofData: data))")
+
+		// upload random image
+		let resultDictionary = try await uploadcare.uploadAPI.directUploadInForeground(files: ["random_file_name.jpg": data], store: .doNotStore)
+		guard let uuid = resultDictionary.values.first else {
+			XCTFail()
+			return
+		}
+
+		try await Task.sleep(nanoseconds: 1 * NSEC_PER_SEC)
+
+		let documentInfo = try await uploadcare.documentInfo(uuid)
+
+		XCTAssertNil(documentInfo.error)
+		XCTAssertFalse(documentInfo.format.name.isEmpty)
+		XCTAssertFalse(documentInfo.format.conversionFormats.isEmpty)
+
+		print(documentInfo)
+	}
 }
